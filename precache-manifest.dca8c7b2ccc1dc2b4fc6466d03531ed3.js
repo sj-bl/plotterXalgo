a5829461cafc48bb6f01739472b478b62bde02c7
sj-bl <sj.gamersnation@gmail.com>
2020-07-29 12:29:15 +0530

    Updates

---
+++ precache-manifest.dca8c7b2ccc1dc2b4fc6466d03531ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d97e45c6c9249679382f7011ed6d50",
+    "url": "/plotterXalgo/index.html"
+  },
+  {
+    "revision": "1ee4259d5b3f7695f911",
+    "url": "/plotterXalgo/static/css/main.3a3623b8.chunk.css"
+  },
+  {
+    "revision": "ab07e95cea84b666bc30",
+    "url": "/plotterXalgo/static/js/2.ba82914d.chunk.js"
+  },
+  {
+    "revision": "a13369e7405157f254f1ffcc4cc651e5",
+    "url": "/plotterXalgo/static/js/2.ba82914d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee4259d5b3f7695f911",
+    "url": "/plotterXalgo/static/js/main.be12586c.chunk.js"
+  },
+  {
+    "revision": "65ccb0be0fe036629481",
+    "url": "/plotterXalgo/static/js/runtime-main.d2921498.js"
+  }
+]);
